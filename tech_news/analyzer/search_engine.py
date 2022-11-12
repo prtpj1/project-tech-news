acc18451d3f3e1bf77cf30b2578a079c786014b2
prtpj1 <prtpj1@gmail.com>
2022-11-12 17:49:59 -0300

    :nerd_face: refactor:(Requisites 6, 7, 8 and 9) add method format_news:arrow_right: remove the var format_news and change it to a method which repeat the same function inside methods: search_by_title, search_by_date, search_by_tag and search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,12 +2,15 @@ from tech_news.database import search_news
 from datetime import datetime
 
 
+def format_news(searched_news):
+    return [(news["title"], news["url"]) for news in searched_news]
+
+
 # Requisito 6
 def search_by_title(title):
     searched_news = search_news({"title": {"$regex": title, "$options": "i"}})
-    format_news = [(news["title"], news["url"]) for news in searched_news]
 
-    return format_news
+    return format_news(searched_news)
 
 
 # Requisito 7
@@ -15,9 +18,9 @@ def search_by_date(date):
     try:
         format_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
         searched_news = search_news({"timestamp": {"$eq": format_date}})
-        format_news = [(news["title"], news["url"]) for news in searched_news]
 
-        return format_news
+        return format_news(searched_news)
+
     except ValueError:
         raise ValueError("Data inválida")
 
@@ -25,9 +28,8 @@ def search_by_date(date):
 # Requisito 8
 def search_by_tag(tag):
     searched_news = search_news({"tags": {"$regex": tag, "$options": "i"}})
-    format_news = [(news["title"], news["url"]) for news in searched_news]
 
-    return format_news
+    return format_news(searched_news)
 
 
 # Requisito 9
@@ -35,6 +37,5 @@ def search_by_category(category):
     searched_news = search_news(
         {"category": {"$regex": category, "$options": "i"}}
     )
-    format_news = [(news["title"], news["url"]) for news in searched_news]
 
-    return format_news
+    return format_news(searched_news)
