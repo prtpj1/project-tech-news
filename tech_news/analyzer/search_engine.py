8d7b1c0a381d69dbaf71e85b03e6bdd1cbfe5324
prtpj1 <prtpj1@gmail.com>
2024-10-23 18:39:35 -0300

    :nerd_face: refactor: Improves data processing :arrow_right: Improves data processing and presentation of news for better clarity

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,25 +1,69 @@
 from tech_news.database import search_news
 from datetime import datetime
 
+# import json
+from colorama import init, Fore
+
+init(autoreset=True)
+
 
 def format_news(searched_news):
-    return [(news["title"], news["url"]) for news in searched_news]
+    return [
+        {"title": news["title"], "url": news["url"]} for news in searched_news
+    ]
+
+
+def colored_news(news_list):
+    total_news = len(news_list)
+
+    if not news_list:
+        print(
+            (
+                f"{Fore.RED} Nenhuma noticia encontrada. "
+                "Tente outra palavra ou data."
+            )
+        )
+
+    for news in news_list:
+        print(f"{Fore.WHITE}Title: {Fore.YELLOW}{news['title']}")
+        print(f"{Fore.WHITE}URL: {Fore.LIGHTBLUE_EX}{news['url']}\n")
+
+    print(
+        (
+            f"{Fore.WHITE}Total de noticias encontradas: "
+            f"{Fore.LIGHTGREEN_EX}{total_news}"
+        )
+    )
 
 
 # Requisito 6
 def search_by_title(title):
     searched_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    formatted_news = format_news(searched_news)
 
-    return format_news(searched_news)
+    colored_news(formatted_news)
+    return ""
 
 
 # Requisito 7
 def search_by_date(date):
     try:
-        format_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
-        searched_news = search_news({"timestamp": {"$eq": format_date}})
+        if "/" in date:
+            day, month, year = date.split("/")
+        elif "-" in date:
+            day, month, year = date.split("-")
+        else:
+            raise ValueError("Formato de data inválido")
 
-        return format_news(searched_news)
+        iso_date = f"{year}-{month}-{day}"
+
+        format_date = datetime.fromisoformat(iso_date).strftime("%d/%m/%Y")
+        searched_news = search_news({"timestamp": {"$eq": format_date}})
+        formatted_searched_news = format_news(searched_news)
+
+        colored_news(formatted_searched_news)
+
+        return ""
 
     except ValueError:
         raise ValueError("Data inválida")
@@ -28,8 +72,11 @@ def search_by_date(date):
 # Requisito 8
 def search_by_tag(tag):
     searched_news = search_news({"tags": {"$regex": tag, "$options": "i"}})
+    formatted_news = format_news(searched_news)
 
-    return format_news(searched_news)
+    colored_news(formatted_news)
+
+    return ""
 
 
 # Requisito 9
@@ -37,5 +84,8 @@ def search_by_category(category):
     searched_news = search_news(
         {"category": {"$regex": category, "$options": "i"}}
     )
+    formatted_news = format_news(searched_news)
 
-    return format_news(searched_news)
+    colored_news(formatted_news)
+
+    return ""
